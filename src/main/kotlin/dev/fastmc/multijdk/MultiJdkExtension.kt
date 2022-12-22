0f3b48dfdf67d0ae5efdebaea0b5642cf2c0b58e
Luna <62033805+Luna5ama@users.noreply.github.com>
2022-12-22 04:07:52 -0500

    Updated 1.1.1

--- src/main/kotlin/dev/fastmc/multijdk/MultiJdkExtension.kt
+++ src/main/kotlin/dev/fastmc/multijdk/MultiJdkExtension.kt
@@ -2,6 +2,8 @@ package dev.fastmc.multijdk
 
 import org.gradle.api.Action
 import org.gradle.api.Project
+import org.gradle.api.attributes.Attribute
+import org.gradle.api.component.AdhocComponentWithVariants
 import org.gradle.api.file.SourceDirectorySet
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.publish.PublishingExtension
@@ -16,38 +18,87 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import javax.inject.Inject
 
 @Suppress("DuplicatedCode", "unused")
-abstract class MultiJdkExtension {
+abstract class MultiJdkExtension @Inject constructor(private val component: AdhocComponentWithVariants) {
     @get:Inject
     protected abstract val project: Project
 
-    fun defaultJavaVersion(version: JavaLanguageVersion) {
-        project.sourceSets.getByName("main").configureForJavaVersion(version)
-    }
+    private val sourceSets0 = mutableMapOf<JavaLanguageVersion, SourceSet>()
 
-    fun newJavaVersion(version: JavaLanguageVersion) {
-        val javaVersionName = "java${version.asInt()}"
-        project.java.registerFeature(javaVersionName) { feature ->
-            project.pluginManager.withPlugin("maven-publish") {
-                project.publishing.publications.withType(MavenPublication::class.java).configureEach {
-                    it.suppressPomMetadataWarningsFor("${javaVersionName}ApiElements")
-                    it.suppressPomMetadataWarningsFor("${javaVersionName}RuntimeElements")
-                }
+    lateinit var base: SourceSet; private set
+    val sourceSets: Map<JavaLanguageVersion, SourceSet> get() = sourceSets0
+
+    fun baseJavaVersion(version: JavaLanguageVersion) {
+        base = getOrRegister(version).apply {
+            project.afterEvaluate {
+                it.sourceSets.getByName("main").compileClasspath += this.compileClasspath
             }
-            feature.capability(
-                project.group.toString(),
-                "${project.name}-$javaVersionName",
-                project.version.toString()
-            )
-            feature.usingSourceSet(project.sourceSets.create(javaVersionName).configureForJavaVersion(version))
         }
     }
 
-    private fun SourceSet.configureForJavaVersion(javaVersion: JavaLanguageVersion): SourceSet {
-        val fullJavaVersion =
-            if (javaVersion.asInt() <= 8) "1.${javaVersion.asInt()}" else javaVersion.asInt().toString()
+    fun newJavaVersion(version: JavaLanguageVersion) {
+        getOrRegister(version)
+    }
+
+    private fun getOrRegister(version: JavaLanguageVersion): SourceSet {
+        var sourceSet = sourceSets0[version]
+        if (sourceSet == null) {
+            sourceSet = project.sourceSets.create(version.javaName).configureForJavaVersion(version)
+            sourceSets0[version] = sourceSet
+
+            project.java.registerFeature(version.javaName) { f ->
+                project.pluginManager.withPlugin("maven-publish") {
+                    project.publishing.publications.withType(MavenPublication::class.java).configureEach {
+                        it.suppressPomMetadataWarningsFor("${version.javaName}ApiElements")
+                        it.suppressPomMetadataWarningsFor("${version.javaName}ApiElementsClasses")
+                        it.suppressPomMetadataWarningsFor("${version.javaName}RuntimeElements")
+                    }
+                }
+                f.capability(
+                    project.group.toString(),
+                    "${project.name}-${version.javaName}",
+                    project.version.toString()
+                )
+                f.usingSourceSet(sourceSet)
+            }
+
+            configureComponent(version)
+        }
+
+        return sourceSet
+    }
+
+    private fun configureComponent(version: JavaLanguageVersion) {
+        val apiElements = project.configurations.getByName("${version.javaName}ApiElements").attributes { sub ->
+            project.configurations.getByName("apiElements").attributes.let { root ->
+                root.keySet().forEach {
+                    if (it.name == "org.gradle.jvm.version") return@forEach
+                    @Suppress("UNCHECKED_CAST")
+                    sub.attribute(it as Attribute<Any>, root.getAttribute(it)!!)
+                }
+            }
+        }
+        component.addVariantsFromConfiguration(apiElements) {
+            it.mapToMavenScope("compile")
+        }
+        val runtimeElement =
+            project.configurations.getByName("${version.javaName}RuntimeElements").attributes { sub ->
+                project.configurations.getByName("runtimeElements").attributes.let { root ->
+                    root.keySet().forEach {
+                        if (it.name == "org.gradle.jvm.version") return@forEach
+                        @Suppress("UNCHECKED_CAST")
+                        sub.attribute(it as Attribute<Any>, root.getAttribute(it)!!)
+                    }
+                }
+            }
+        component.addVariantsFromConfiguration(runtimeElement) {
+            it.mapToMavenScope("runtime")
+        }
+    }
+
+    private fun SourceSet.configureForJavaVersion(version: JavaLanguageVersion): SourceSet {
         project.afterEvaluate {
             val jarTask = project.tasks.named(jarTaskName, Jar::class.java) {
-                it.archiveClassifier.set("java${javaVersion.asInt()}")
+                it.archiveClassifier.set(version.javaName)
             }
             project.artifacts {
                 it.add("archives", jarTask)
@@ -57,15 +108,46 @@ abstract class MultiJdkExtension {
         val mainSourceSet = project.sourceSets.getByName("main")
 
         if (this != mainSourceSet) {
-            compileClasspath += mainSourceSet.compileClasspath
+            project.afterEvaluate {
+                fun extendConfigurationFrom(configurationNameFunction: (SourceSet) -> String) {
+                    val root = it.configurations.findByName(configurationNameFunction(mainSourceSet))
+                    val sub = it.configurations.findByName(configurationNameFunction(this))
+                    if (root != null && sub != null) {
+                        sub.dependencies.addAll(root.dependencies)
+                    }
+                }
+
+                extendConfigurationFrom(SourceSet::getCompileOnlyConfigurationName)
+                extendConfigurationFrom(SourceSet::getCompileOnlyApiConfigurationName)
+                extendConfigurationFrom(SourceSet::getCompileClasspathConfigurationName)
+                extendConfigurationFrom(SourceSet::getAnnotationProcessorConfigurationName)
+                extendConfigurationFrom(SourceSet::getApiConfigurationName)
+                extendConfigurationFrom(SourceSet::getImplementationConfigurationName)
+                extendConfigurationFrom(SourceSet::getApiElementsConfigurationName)
+                extendConfigurationFrom(SourceSet::getRuntimeOnlyConfigurationName)
+                extendConfigurationFrom(SourceSet::getRuntimeClasspathConfigurationName)
+                extendConfigurationFrom(SourceSet::getRuntimeElementsConfigurationName)
+                extendConfigurationFrom(SourceSet::getJavadocElementsConfigurationName)
+                extendConfigurationFrom(SourceSet::getSourcesElementsConfigurationName)
+            }
 
             resources.srcDirs(mainSourceSet.resources.srcDirs)
             java.srcDirs(mainSourceSet.java.srcDirs)
             kotlin.srcDirs(mainSourceSet.kotlin.srcDirs)
         }
 
+        configureJavaCompile(version, mainSourceSet)
+        configureKotlinCompile(version, mainSourceSet)
+
+        return this
+    }
+
+    private fun SourceSet.configureJavaCompile(
+        version: JavaLanguageVersion,
+        mainSourceSet: SourceSet
+    ) {
         project.tasks.named(compileJavaTaskName, JavaCompile::class.java) { javaCompile ->
-            javaCompile.javaCompiler.set(project.javaToolchains.compilerFor { it.languageVersion.set(javaVersion) })
+            javaCompile.javaCompiler.set(project.javaToolchains.compilerFor { it.languageVersion.set(version) })
             javaCompile.options.let { sub ->
                 if (this != mainSourceSet) {
                     project.tasks.named(mainSourceSet.compileJavaTaskName, JavaCompile::class.java)
@@ -80,16 +162,21 @@ abstract class MultiJdkExtension {
                             sub.isFailOnError = root.isFailOnError
                         }
                 }
-                javaCompile.sourceCompatibility = fullJavaVersion
-                javaCompile.targetCompatibility = fullJavaVersion
+                javaCompile.sourceCompatibility = version.fullJavaVersion
+                javaCompile.targetCompatibility = version.fullJavaVersion
             }
         }
+    }
 
+    private fun SourceSet.configureKotlinCompile(
+        version: JavaLanguageVersion,
+        mainSourceSet: SourceSet
+    ) {
         project.tasks.named(
             getCompileTaskName("kotlin"),
             KotlinCompile::class.java
         ) { kotlinCompile ->
-            kotlinCompile.kotlinJavaToolchain.toolchain.use(project.javaToolchains.launcherFor { it.languageVersion.set(javaVersion) })
+            kotlinCompile.kotlinJavaToolchain.toolchain.use(project.javaToolchains.launcherFor { it.languageVersion.set(version) })
             kotlinCompile.kotlinOptions.let { sub ->
                 if (this != mainSourceSet) {
                     project.tasks.named(
@@ -109,11 +196,9 @@ abstract class MultiJdkExtension {
                         sub.verbose = root.verbose
                     }
                 }
-                sub.jvmTarget = fullJavaVersion
+                sub.jvmTarget = version.fullJavaVersion
             }
         }
-
-        return this
     }
 
     private val SourceSet.kotlin
@@ -137,4 +222,7 @@ abstract class MultiJdkExtension {
 
     private val JavaLanguageVersion.fullJavaVersion: String
         get() = if (this <= JavaLanguageVersion.of(8)) "1.$this" else this.toString()
+
+    private val JavaLanguageVersion.javaName: String
+        get() = "java${this.asInt()}"
 }
--- src/main/kotlin/dev/fastmc/multijdk/MultiJdkPlugin.kt
+++ src/main/kotlin/dev/fastmc/multijdk/MultiJdkPlugin.kt
@@ -2,10 +2,33 @@ package dev.fastmc.multijdk
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.component.SoftwareComponentFactory
+import javax.inject.Inject
 
 @Suppress("unused")
-class MultiJdkPlugin : Plugin<Project> {
+abstract class MultiJdkPlugin : Plugin<Project> {
+    @get:Inject
+    abstract val softwareComponentFactory: SoftwareComponentFactory
+
     override fun apply(project: Project) {
-        project.extensions.create("multiJdk", MultiJdkExtension::class.java)
+        val component = softwareComponentFactory.adhoc("multi-jdk")
+        project.components.add(component)
+        project.afterEvaluate {
+            project.configurations.findByName("sourcesElements")?.let { sourcesElements ->
+                component.addVariantsFromConfiguration(sourcesElements) {
+                    it.mapToMavenScope("runtime")
+                }
+            }
+        }
+        project.tasks.named("compileJava").configure {
+            it.enabled = false
+        }
+        project.tasks.named("compileKotlin").configure {
+            it.enabled = false
+        }
+        project.tasks.named("jar").configure {
+            it.enabled = false
+        }
+        project.extensions.create("multiJdk", MultiJdkExtension::class.java, component)
     }
 }
